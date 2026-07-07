0e1572da5f4027bd7665c6958bd7327ffc0c2a9c
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-07-07 16:40:36 +0300

    Task 6 Solve

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
-int main (void){
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
-int i ;
-double d;
-char c ;
-printf("The size of an int is : %lu.\n", (unsigned long)sizeof(i));
-printf("The size of an double is : %lu./n", (unsigned long)sizeof(d));
-printf("The size od an char is :%lu./n" , (unsigned long)sizeof(c));
-
-return(0);
-
+	return (0);
 }
