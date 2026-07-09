6c56c0805450830a7d7a1260ef015ddeb440d9c7
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-07-09 15:09:56 +0300

    Task 1 solve

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n>5)
+	{
+	printf( "%d the string and is greater than 5\n",n);	
+	}
+		else if(n == 0)
+	 {
+			printf ( "%d and is greater than 5\n",n);	  
+	 }
+		else if(n < 6 && n != 0)
+	 {
+			printf( "%d and is less than 6 and not 0 \n",n);
+	 } 
+	return (0);
+}
