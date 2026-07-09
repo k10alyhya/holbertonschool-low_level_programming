2a65ea9041d768bcf15a1cfdd3308eee3092391e
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-07-09 13:12:30 +0300

    Task 0 Solve

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,16 +10,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 10){
-	 printf("%d is positive\n", n);
+	if (n > 10)
+	{
+		printf("is positive %d \n", n);
 	}
-		else if (n == 0){
-		printf("%d is zero\n", n);
+	else if (n == 0)
+	{
+		printf("is zero %d \n", n);
 	}
-
-		else if (n < 0){
-		printf("%d is negative\n", n);
+	else if (n < 0)
+	{
+		printf("is Negative %d \n", n);
 	}
-
 	return (0);
 }
