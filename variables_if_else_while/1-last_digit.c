db5ea426cb945bf9ef58264200ad1f3e735ec836
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-07-09 15:25:38 +0300

    task 1 solve

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/**
+ * main - Prints the last digit of a randomly generated number
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
