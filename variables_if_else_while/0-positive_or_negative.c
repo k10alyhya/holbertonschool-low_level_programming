cefaea2c48d80a66832d47b4cb77687cad50190f
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-07-09 13:17:01 +0300

    Task 0 Solve

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - prints whether a random number is positive, zero, or negative
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
 	int n;
