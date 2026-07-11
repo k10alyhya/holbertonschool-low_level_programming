25504e264f94eff375cf874c0048eb5ed1914088
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-07-11 21:27:16 +0300

    task 10 solve

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: the size of the triangle
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int row, spaces, hashes;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (spaces = size - row; spaces > 0; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
+}
