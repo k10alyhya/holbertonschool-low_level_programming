f61ae51e0d333f746e4c062a4d92f730eb92ccc5
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-07-11 18:14:32 +0300

    Task 3 Solve

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	int c;
+
+	c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
