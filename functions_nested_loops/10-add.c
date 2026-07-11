c8baa171efddcf4ed3979824e3ed839c2f93191f
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-07-11 13:26:05 +0300

    Task 10 Solve

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @x: the first integer to be added
+ * @n: the second integer to be added
+ *
+ * Return: the sum of x and n
+ */
+int add(int x, int n)
+{
+	return (x + n);
+}
