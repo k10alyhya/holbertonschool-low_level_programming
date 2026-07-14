b2360b6d0cbe79c224d696359f9bfe8c5f8cd919
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-07-14 16:32:35 +0300

    task 1 solve

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * swap_int - update value of two integers.
+ * @a : pointer to an integer.
+ * @b : pointer to an integer.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 10;
+	*b = 15;
+}
