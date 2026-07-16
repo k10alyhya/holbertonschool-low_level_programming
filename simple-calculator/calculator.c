af5130820ab4562767accce52251577e6fe81327
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-07-16 14:15:14 +0300

    task 1 solve

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -6,17 +6,37 @@
  */
 int main (void)
 {
-	int n;
-
-	printf(" Welcome to Simple Calculator ");
-	printf(" Simple Calculator : \n   1) Add \n  2) Subtract \n  3) Multiply \n 4) Divide \n  0) Quit \n Choice :");
-
-	scanf(" %d", &n);
+	int n = 1;
 	
-	if (n == 0)
+	 
+	printf(" Welcome to Simple Calculator ");
+	
+
+
+	while (n != 0)
 	{
-		printf("!bye\n");
+		printf(" Simple Calculator : \n   1) Add \n  2) Subtract \n  3) Multiply \n 4) Divide \n  0) Quit \n Choice :");
+
+		scanf(" %d", &n);
+
+		if (n == 0)
+		{
+			printf("!bey\n");
+		
+		}
+
+		else if (n > 4  || n < 0)
+		{
+			printf ("  Invalid choice");
+		}
 	}
+
+	{
+
+	}
+	
+	
+
 
 	return (0); 
 }
