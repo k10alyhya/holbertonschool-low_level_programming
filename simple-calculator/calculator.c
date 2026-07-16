e944840dbdb4ca52f7fc50bdf0a9cb91015b3c99
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-07-16 15:07:18 +0300

    Task 3 and 4 solve

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -12,7 +12,6 @@ int main (void)
 	printf(" Welcome to Simple Calculator ");
 	
 
-
 	while (n != 0)
 	{
 		printf(" Simple Calculator : \n   1) Add \n  2) Subtract \n  3) Multiply \n 4) Divide \n  0) Quit \n Choice :");
@@ -29,6 +28,68 @@ int main (void)
 		{
 			printf ("  Invalid choice");
 		}
+
+		else if (n == 1) 
+		{
+			double num1 , num2;
+			double result ;
+			scanf("%lf", &num1);
+			printf("A: %f.2\n", num1);
+
+			scanf("%lf", &num2);
+
+			 result = num1 + num2;
+
+			printf("Result = %.2f\n", result);
+			
+		}
+		else if (n == 2)
+{
+			double num1, num2;
+			double result;
+
+			scanf("%lf", &num1);
+			printf("A: %.2f\n", num1);
+
+			scanf("%lf", &num2);
+			printf("B: %.2f\n", num2);
+
+			result = num1 - num2;
+
+			printf("Result = %.2f\n", result);
+}
+
+else if (n == 3)
+{
+	double num1, num2;
+	double result;
+
+	scanf("%lf", &num1);
+	printf("A: %.2f\n", num1);
+
+	scanf("%lf", &num2);
+	printf("B: %.2f\n", num2);
+
+	result = num1 * num2;
+
+	printf("Result = %.2f\n", result);
+}
+
+else if (n == 4)
+{
+	double num1, num2;
+	double result;
+
+	scanf("%lf", &num1);
+	printf("A: %.2f\n", num1);
+
+	scanf("%lf", &num2);
+	printf("B: %.2f\n", num2);
+
+	result = num1 / num2;
+
+	printf("Result = %.2f\n", result);
+}
 	}
 
 	{
