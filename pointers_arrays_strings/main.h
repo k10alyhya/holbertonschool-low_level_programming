47ede7ef9f64f2f89cdd293e175e59d87754891a
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-07-18 21:48:18 +0300

    task 2 solve

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -23,6 +23,8 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *_memset(char *s, char b, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 
 
 #endif
