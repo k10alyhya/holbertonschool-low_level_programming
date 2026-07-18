0c20da51f9876d9a0100764614c928ddd1dd9fc2
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-07-18 16:29:00 +0300

    task 4 solve

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,24 +1,22 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_array - print an array of integers
- * @a: an array of integers
- * @n: the number of elements to swap
- *
- * Return: nothing.
+ * reverse_array - reverses an array of integers
+ * @a: array of integers
+ * @n: number of elements in the array
  */
- void reverse_array(int *a, int n)
+void reverse_array(int *a, int n)
 {
 	int tmp;
-	int start = 0; 
+	int start = 0;
 	int end = n - 1;
 
 	while (start < end)
 	{
 		tmp = a[start];
 		a[start] = a[end];
-		a[end] = tmp; 
+		a[end] = tmp;
+
 		start++;
 		end--;
 	}
