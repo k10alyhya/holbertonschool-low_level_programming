3c084698b14b9fcbd2f4cb0467b7a4f516724065
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-07-25 18:23:24 +0300

    task 2 solve

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - stores information about a dog
  * @name: name of the dog
@@ -12,7 +13,8 @@ struct dog
 	float age;
 	char *owner;
 };
- void init_dog(struct dog *d, char *name, float age, char *owner);
- void print_dog(struct dog *d);
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif /* DOG_H */
