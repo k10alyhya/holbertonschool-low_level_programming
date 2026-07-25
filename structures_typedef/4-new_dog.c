83598217209fba95b5cab34d799af4eb0f487382
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-07-25 19:41:49 +0300

    task 4 solve

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
  * struct dog - stores information about a dog
  * @name: name of the dog
@@ -18,5 +17,5 @@ typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+dog_t *new_dog(char *name, float age, char *owner);
 #endif /* DOG_H */
