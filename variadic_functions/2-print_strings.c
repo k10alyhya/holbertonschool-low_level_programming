1021cab0ea62f691e5317b0ad1e7e64aa70cb5c6
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-07-29 19:59:04 +0300

    task 2 solve

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -8,13 +8,14 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i = 0;
+	char *str;
 	va_list args;
 
 	va_start(args, n);
 
 	while (i < n)
 	{
-		char *str = va_arg(args, char *);
+		str = va_arg(args, char *);
 
 		if (str == NULL)
 			printf("(nil)");
