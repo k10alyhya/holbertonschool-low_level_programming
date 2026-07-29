1e2d01c6dc4489ac7000f4518cd92b8e18a525b8
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-07-29 15:45:23 +0300

    task 3 solve

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+#include <string.h>
+
+/**
+ * get_op_func - Selects the correct arithmetic function
+ * @s: The operator passed to the program
+ *
+ * Return: Pointer to the matching function, or NULL if no match is found
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
