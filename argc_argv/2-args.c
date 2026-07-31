ab002a1e3e39574207674f686e830055d322c411
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-07-31 21:15:36 +0300

    task 2 solve

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
-int main (int argc , char * argv[])
+
+/**
+ * main - prints all arguments received
+ * @argc: number of command-line arguments
+ * @argv: array containing command-line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
 {
 	int i = 0;
 
-		while ( i  < argc){
-			printf("%s\n" , argv[i]);
-			i++;
-		}
-        return(0);
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+
+	return (0);
 }
