2b636f69a1635de2a1878b550b0626c10e5492a4
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-07-31 20:10:46 +0300

    task 0 solve

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 /**
- * main - print the file name
+ * main - prints the name of the program
+ * @argc: number of command-line arguments
+ * @argv: array containing command-line arguments
+ *
+ * Return: Always 0
  */
-
 int main(int argc, char *argv[])
 {
 	(void)argc;
+
 	printf("%s\n", argv[0]);
+
 	return (0);
 }
